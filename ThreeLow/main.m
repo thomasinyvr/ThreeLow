b2ac5127fdc3fe7bc178ff8cb9441154b1db4c83
thomasinyvr <tomfriesman@gmail.com>
2016-03-09 11:19:17 -0800

    Initial Commit

---
+++ ThreeLow/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ThreeLow
+//
+//  Created by Thomas Friesman on 2016-03-09.
+//  Copyright © 2016 Thomas Friesman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
